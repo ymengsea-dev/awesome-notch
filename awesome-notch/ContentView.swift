97204e266cc381630d06d151d94ca1ed9c8532d0
ymengsea <mrmengsea@gmail.com>
2026-01-08 14:38:58 +0700

    Initial Commit

---
+++ awesome-notch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  awesome-notch
+//
+//  Created by MacBook on 1/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ awesome-notch/awesome_notchApp.swift
@@ -0,0 +1,17 @@
+//
+//  awesome_notchApp.swift
+//  awesome-notch
+//
+//  Created by MacBook on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct awesome_notchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
